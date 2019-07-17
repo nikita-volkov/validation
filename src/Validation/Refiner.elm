933198e52ab21a2bedd016c5a530f635dac10be5
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-07-17 17:17:45 +0300

    Reimplement with a single error

--- src/Validation/Refiner.elm
+++ src/Validation/Refiner.elm
@@ -8,20 +8,17 @@ import Validation.Validator as Validator exposing (Validator)
 Projection from `input` to `output`,
 where `input` values can be of a broader range and thus fail to produce `output`.
 
-Implemented as a mapping function which can fail and list the errors.
+Implemented as a mapping function which can fail.
 
 This abstraction can be composed in all sorts of ways: monadically, alternatively, categorically.
 -}
-type alias Refiner error input output = input -> Result (List error) output
+type alias Refiner error input output = input -> Result error output
 
 succeed : output -> Refiner error input output
 succeed output _ = Ok output
 
 fail : error -> Refiner error input output
-fail error _ = Err [error]
-
-none : Refiner error input output
-none _ = Err []
+fail error _ = Err error
 
 {-|
 Lift a validator into a refiner,
@@ -30,8 +27,8 @@ without mapping it into another type.
 -}
 validate : Validator error value -> Refiner error value value
 validate validator value = case validator value of
-  [] -> Ok value
-  errors -> Err errors
+  error :: _ -> Err error
+  _ -> Ok value
 
 identity : Refiner error a a
 identity = Ok
@@ -46,20 +43,6 @@ which produces `output` from `input` without errors.
 map : (input -> output) -> Refiner error input output
 map fn input = Ok (fn input)
 
-{-|
-Iterate thru refiners until the first one that succeeds,
-collecting all errrors if all of them fail.
--}
-any : List (Refiner error input output) -> Refiner error input output
-any list input =
-  let
-    loop errList currentRefinerList = case currentRefinerList of
-      currentRefiner :: remainingRefinerList -> case currentRefiner input of
-        Ok output -> Ok output
-        Err currentErrList -> loop (currentErrList ++ errList) remainingRefinerList
-      _ -> Err errList
-    in loop [] list
-
 mapInput : (b -> a) -> Refiner error a output -> Refiner error b output
 mapInput fn refinerA = fn >> refinerA
 
@@ -67,10 +50,44 @@ mapOutput : (a -> b) -> Refiner error input a -> Refiner error input b
 mapOutput fn refinerA = refinerA >> Result.map fn
 
 mapError : (a -> b) -> Refiner a input output -> Refiner b input output
-mapError fn refinerA = refinerA >> Result.mapError (List.map fn)
+mapError fn refinerA = refinerA >> Result.mapError fn
 
 {-|
 Sequentially execute two refiners on the same input.
 -}
 andThen : (a -> Refiner error input b) -> Refiner error input a -> Refiner error input b
 andThen fn refinerA input = refinerA input |> Result.andThen (\ a -> fn a input)
+
+{-|
+Try one refiner and, if it fails, the other.
+The one to the right goes first.
+-}
+or : Refiner error input output -> Refiner error input output -> Refiner error input output
+or that this input = case this input of
+  Ok output -> Ok output
+  _ -> that input
+
+{-|
+Try a refiner or iterate thru a list of alternatives until the first one that succeeds,
+using the error of the last one of them if all fail.
+-}
+orAny : List (Refiner error input output) -> Refiner error input output -> Refiner error input output
+orAny tail head input = case head input of
+  Ok output -> Ok output
+  Err error -> any error tail input
+
+{-|
+Iterate thru refiners until the first one that succeeds,
+using the error of the last one of them if all fail or
+the specified one when the list is empty.
+-}
+any : error -> List (Refiner error input output) -> Refiner error input output
+any error refiners input =
+  let
+    loop currentError currentRefiners = case currentRefiners of
+      refiner :: nextRefiners -> case refiner input of
+        Ok output -> Ok output
+        Err nextError -> loop nextError nextRefiners
+      _ -> Err currentError
+    in loop error refiners
+
